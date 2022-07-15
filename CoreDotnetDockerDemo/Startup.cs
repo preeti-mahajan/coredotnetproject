599b977d7381fd892e6fee396e25c9066dee531b
Preeti Mahajan <preeti.a.mahajan@accenture.com>
2022-07-15 22:35:57 +0530

    Update Startup.cs

--- CoreDotnetDockerDemo/Startup.cs
+++ CoreDotnetDockerDemo/Startup.cs
@@ -15,6 +15,7 @@ namespace CoreDotnetDockerDemo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +25,16 @@ namespace CoreDotnetDockerDemo
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseMvc();
         }
     }
 }
