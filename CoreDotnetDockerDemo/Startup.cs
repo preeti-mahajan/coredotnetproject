0010040e1cf5983ec89371ecd6b0df88a28323d0
Preeti Mahajan <preeti.a.mahajan@accenture.com>
2022-07-16 13:24:01 +0530

    Update Startup.cs

--- CoreDotnetDockerDemo/Startup.cs
+++ CoreDotnetDockerDemo/Startup.cs
@@ -16,7 +16,7 @@ namespace CoreDotnetDockerDemo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
